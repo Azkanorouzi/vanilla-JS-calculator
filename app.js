f7a4e96788589f69ae37ddfe2916a18a15adf1a4
Azka <a.azkanorouzi@gmail.com>
2022-10-01 20:36:08 +0330

    show operationResult

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ let preOperation ;
 let currentOperation;
 let lastKeyIndex;
 let lastKey;
+let operationResult;
 /* Keys click event */
 let clickedKeyName;
 keys.forEach(key => {
@@ -67,20 +68,90 @@ function populateScreen(clickedKey) {
       /* clearing out the pre operation */
       currentOperationEl.textContent = " ";
     }
-    operation = preOperationEl.textContent.split(" ");
     if (clickedKey === '=') {
-      operation.pop();
+      operation = preOperationEl.textContent
+      /* Calculating the result */
       console.log(operation);
+      operationResult = operate(operation)
+      if (operationResult === Infinity) {
+        operationResult ='WTF !^&*(*&^%%';
+      }
+      /* Showing the result */
+      preOperationEl.textContent = ""
+      currentOperationEl.textContent=operationResult;
     }
   }
   /* When user hits = */
   /* When user hits del button */
   else if (clickedKey === 'del') {
     currentOperationEl.textContent = currentOperationEl.textContent.slice(0,-1);
+  } else if (clickedKey === 'reset') {
+    reset();
   }
   else {
     currentOperationEl.textContent += clickedKey;
-    number
   }
 }
 
+
+/* This function will get a string as argument and act exactly like eval it will calculate the operation and return the result */
+function operate(operation) {
+  // turning operation into an array
+  operation = operation.split(' ')
+
+  // stores the result of operation
+  let operationResult;
+  // this loop will go through every element of the array and operates every * or / operation and shrinks the string down until there is no more * or / left
+  while (operation.includes('/') || operation.includes('*')) {
+    for (let i = 0; i < operation.length; i++) {
+      if (operation[i] === '*') {
+        operation[i] = operation[i + 1];
+        operation[i] *= operation[i - 1];
+        operation.splice(i + 1, 1)
+        operation.splice(i - 1, 1)
+      }
+      if (operation[i] === '/') {
+        operation[i] = operation[i - 1] / operation[i + 1];
+        operation.splice(i + 1, 1)
+        operation.splice(i - 1, 1)
+      }
+    }
+  }
+  // this loop will go through every element of the array and operates every - or + operation and shrinks the string down until there is no more - or + left
+  while (operation.includes('+') || operation.includes('-')) {
+    for (let i = 0; i < operation.length; i++) {
+      if (operation[i] === '+') {
+        operation[i + 1] = Number(operation[i + 1]);
+        operation[i - 1] = Number(operation[i - 1]);
+        operation[i] = operation[i + 1];
+        operation[i] += operation[i - 1];
+        operation.splice(i + 1, 1)
+        operation.splice(i - 1, 1)
+      }
+      if (operation[i] === '-') {
+        operation[i] = operation[i - 1];
+        operation[i] -= operation[i + 1];
+        operation.splice(i + 1, 1)
+        operation.splice(i - 1, 1)
+      }
+    }
+  }
+  /* Checking to see if the result have a decimal point if so round it up to two decimal points for better readability else just set operation result to it's original value*/
+  if (operation[0] % 1 !== 0) {
+    operationResult = Math.round(operation[0] * 100) / 100;
+  } else {
+    operationResult = operation[0];
+  }
+  return operationResult;
+}
+/* This function will reset everything */
+function reset() {
+  operation = undefined;
+  preOperation = undefined;
+  currentOperation = undefined;
+  lastKeyIndex = undefined;
+  lastKey = undefined;
+  operationResult = undefined;
+  preOperationEl.textContent = ""
+  currentOperationEl.textContent = "";
+}
